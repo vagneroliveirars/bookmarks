b3b6f83c99a295346138e0e618b7621bf5f7144b
Vagner Silva Oliveira <vagner.oliveirars@gmail.com>
2017-10-04 21:41:53 -0300

    HATEOAS REST Service

---
+++ src/main/java/io/spring/guide/tutorials/bookmarks/controller/BookmarkControllerAdvice.java
@@ -0,0 +1,22 @@
+package io.spring.guide.tutorials.bookmarks.controller;
+
+import org.springframework.hateoas.VndErrors;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import io.spring.guide.tutorials.bookmarks.exception.UserNotFoundException;
+
+@ControllerAdvice
+public class BookmarkControllerAdvice {
+	
+	@ResponseBody
+	@ExceptionHandler(UserNotFoundException.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	VndErrors userNotFoundExceptionHandler(UserNotFoundException ex) {
+		return new VndErrors("error", ex.getMessage());
+	}
+	
+}
